40fa928e4e51a6555f7b82305f7e2ee00a7451ae
BOMBEROSVLH <ubobomberovlh@gmail.com>
2025-07-26 22:33:13 -0500

    Add files via upload

---
+++ 127_0_0_1.sql
@@ -0,0 +1,303 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-06-2025 a las 05:18:53
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `apsystem`
+--
+CREATE DATABASE IF NOT EXISTS `apsystem` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `apsystem`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `photo` varchar(200) NOT NULL,
+  `created_on` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `firstname`, `lastname`, `photo`, `created_on`) VALUES
+(1, 'admin', '$2y$10$D8nWT323iLmEVCUg4nJdOemlH9txz18IF9kdr8V1omfOHkof53cDK', 'BOMBEROS', 'VICTOR LARCO', 'images.jpg', '2019-12-18');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `time_in` time NOT NULL,
+  `status` int(1) NOT NULL,
+  `time_out` time NOT NULL,
+  `num_hr` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `attendance`
+--
+
+INSERT INTO `attendance` (`id`, `employee_id`, `date`, `time_in`, `status`, `time_out`, `num_hr`) VALUES
+(119, 24, '2020-01-07', '10:11:26', 0, '00:00:00', 0),
+(120, 25, '2020-01-07', '10:17:04', 0, '00:00:00', 9.2833333333333),
+(121, 24, '2025-06-07', '02:31:59', 1, '02:32:13', 15.45),
+(122, 26, '2025-06-07', '02:50:53', 1, '00:00:00', 0),
+(123, 27, '2025-06-09', '01:37:37', 1, '01:37:41', 16.366666666667),
+(124, 28, '2025-06-09', '01:43:03', 1, '01:43:23', 16.266666666667);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cashadvance`
+--
+
+CREATE TABLE `cashadvance` (
+  `id` int(11) NOT NULL,
+  `date_advance` date NOT NULL,
+  `employee_id` varchar(15) NOT NULL,
+  `amount` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `deductions`
+--
+
+CREATE TABLE `deductions` (
+  `id` int(11) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `amount` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `deductions`
+--
+
+INSERT INTO `deductions` (`id`, `description`, `amount`) VALUES
+(6, 'Pago de EPS 4%', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `employees`
+--
+
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL,
+  `employee_id` varchar(15) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `address` text NOT NULL,
+  `birthdate` date NOT NULL,
+  `contact_info` varchar(100) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `position_id` int(11) NOT NULL,
+  `schedule_id` int(11) NOT NULL,
+  `photo` varchar(200) NOT NULL,
+  `created_on` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `employees`
+--
+
+INSERT INTO `employees` (`id`, `employee_id`, `firstname`, `lastname`, `address`, `birthdate`, `contact_info`, `gender`, `position_id`, `schedule_id`, `photo`, `created_on`) VALUES
+(24, '70299771', 'Stefany Del Pilar', 'Chavez Arceles', 'Calle 54 N 12-23', '2023-07-12', 'chavezstefa_1@gmail.com', 'Female', 2, 1, '', '2020-01-07'),
+(26, '018572934', 'Luis', 'Rosales', 'Victor Larco', '2000-01-03', 'lrosales1@gmail.com', 'Male', 4, 1, 'images.jpg', '2025-06-07'),
+(27, '70299772', 'Johann Aldin', 'Sernaque Perez', 'mz j Prima', '1994-09-17', 'johannspr@gmail.com', 'Male', 4, 1, 'images.jpg', '2025-06-09'),
+(28, '12345678', 'Renzo', 'Alban Sanchez', 'Paujiles', '2000-07-11', 'ralvan', 'Male', 2, 1, 'images.jpg', '2025-06-09');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `overtime`
+--
+
+CREATE TABLE `overtime` (
+  `id` int(11) NOT NULL,
+  `employee_id` varchar(15) NOT NULL,
+  `hours` double NOT NULL,
+  `rate` double NOT NULL,
+  `date_overtime` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `position`
+--
+
+CREATE TABLE `position` (
+  `id` int(11) NOT NULL,
+  `description` varchar(150) NOT NULL,
+  `rate` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `position`
+--
+
+INSERT INTO `position` (`id`, `description`, `rate`) VALUES
+(2, 'ASPIRANTE 2025', 1),
+(4, 'ASPIRANTE 2026', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `schedules`
+--
+
+CREATE TABLE `schedules` (
+  `id` int(11) NOT NULL,
+  `time_in` time NOT NULL,
+  `time_out` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `schedules`
+--
+
+INSERT INTO `schedules` (`id`, `time_in`, `time_out`) VALUES
+(1, '19:00:00', '22:30:00'),
+(2, '07:00:00', '00:00:00'),
+(3, '13:00:00', '18:00:00'),
+(4, '10:00:00', '19:00:00');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `cashadvance`
+--
+ALTER TABLE `cashadvance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `deductions`
+--
+ALTER TABLE `deductions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `overtime`
+--
+ALTER TABLE `overtime`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `position`
+--
+ALTER TABLE `position`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `schedules`
+--
+ALTER TABLE `schedules`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
+
+--
+-- AUTO_INCREMENT de la tabla `cashadvance`
+--
+ALTER TABLE `cashadvance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `deductions`
+--
+ALTER TABLE `deductions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT de la tabla `overtime`
+--
+ALTER TABLE `overtime`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `position`
+--
+ALTER TABLE `position`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `schedules`
+--
+ALTER TABLE `schedules`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
